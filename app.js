6102203298cf436013329b70153fb287528783f3
Haruno <74740578+thienbm101102@users.noreply.github.com>
2026-04-12 11:47:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -3246,7 +3246,7 @@ const app = {
                         <span class="um-email">${email || 'Tài khoản User'}</span>
                     </div>
                     <a href="javascript:void(0)" class="um-item" onclick="app.openEditProfile()"><i class="fas fa-user-edit"></i> Hồ sơ của tôi</a>
-					<a href="javascript:void(0)" class="um-item" onclick="app.openDashboard()" style="color: #00ffcc;"><i class="fas fa-briefcase"></i> Quản Lý & Túi Đồ</a>
+					<a href="javascript:void(0)" class="um-item hide-on-mobile" onclick="app.openDashboard()" style="color: #00ffcc;"><i class="fas fa-briefcase"></i> Quản Lý & Túi Đồ</a>
                     <a href="javascript:void(0)" class="um-item" onclick="app.openPremiumModal()" style="color: #ffd700;"><i class="fas fa-crown"></i> Nâng cấp Premium</a>
                     <a href="javascript:void(0)" class="um-item um-logout" onclick="app.logout()"><i class="fas fa-sign-out-alt"></i> Đăng xuất</a>
                 </div>
