81b7060475d55ecb6af1fbb726d961d61862c641
Haruno <74740578+thienbm101102@users.noreply.github.com>
2026-06-17 20:05:38 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Đặt tên phiên bản hiện tại (Mỗi lần update web, bạn thay đổi số này)
-const CURRENT_WEB_VERSION = "2.0.23"; 
+const CURRENT_WEB_VERSION = "2.0.24"; 
 
 // Kiểm tra xem máy người dùng đang lưu bản nào
 const userVersion = localStorage.getItem('haruno_web_version');
